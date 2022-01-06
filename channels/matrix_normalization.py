14e302dad08ab9f06408eb98bfe8f39258098503
guavamin <78477521+guavamin@users.noreply.github.com>
2022-01-06 21:22:03 +0800

    Update matrix_normalization.py

--- channels/matrix_normalization.py
+++ channels/matrix_normalization.py
@@ -3,6 +3,7 @@ import numpy as np
 import h5py
 import math
 
+# type in your channel data in the command line
 channel_sequence = h5py.File(sys.argv[1], 'r')
 
 name = channel_sequence.filename.strip('.hdf5')
@@ -29,14 +30,14 @@ H_matrix = np.array(H_r[:, :, :, :, :] + 1j*H_i[:, :, :, :, :])
 print(H_matrix.shape)
 
 
-
+# normalization step follow the instruction mentioned in the paper at page 6
 H_matrix /= np.sqrt(np.sum((H_matrix*H_matrix.conjugate()).real, axis=(1, 2, 3), keepdims=True) /
                     (np.prod(H_matrix.shape[1:4])))
 
-print(np.sum((H_matrix*H_matrix.conjugate()).real, axis=(1, 2, 3), keepdims=True) /
-                    (np.prod(H_matrix.shape[1:4])))
+# print(np.sum((H_matrix*H_matrix.conjugate()).real, axis=(1, 2, 3), keepdims=True) /
+#                     (np.prod(H_matrix.shape[1:4])))
 
-# np.save(f'{name}.npy', H_matrix)
+np.save(f'{name}.npy', H_matrix)
 
-# print('-------finish matrix normalization and successfully save the data-------')
+print('-------finish matrix normalization and successfully save the data-------')
 
